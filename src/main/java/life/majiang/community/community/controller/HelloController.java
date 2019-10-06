4315651619f691e441394b98c83ee74954451406
taoranju <1969778249@qq.com>
2019-10-06 23:15:12 +0800

    one day

---
+++ src/main/java/life/majiang/community/community/controller/HelloController.java
@@ -0,0 +1,27 @@
+package life.majiang.community.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @ProjectName: community
+ * @Package: life.majiang.community.community.controller
+ * @ClassName: HelloController
+ * @Author: 陶然居
+ * @Description:
+ * @Date: 2019/10/6 22:31
+ * @Version: 1.0
+ */
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
