38edff860314fa412dc62fd8045323d9662699da
taoranju <1969778249@qq.com>
2019-10-08 22:20:03 +0800

    保存登录状态

--- src/main/java/life/majiang/community/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/community/controller/IndexController.java
@@ -1,9 +1,15 @@
 package life.majiang.community.community.controller;
 
+import life.majiang.community.community.mapper.UserMapper;
+import life.majiang.community.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @ProjectName: community
@@ -18,6 +24,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class IndexController {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/hello")
     public String hello(@RequestParam(name = "name") String name, Model model) {
         model.addAttribute("name",name);
@@ -25,7 +34,19 @@ public class IndexController {
     }
 
     @GetMapping("/")
-    public String index() {
+    public String index(HttpServletRequest request) {
+
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie:cookies) {
+            if(cookie.getName().equals("token")) {
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if(user != null) {
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
         return "index";
     }
 }
--- src/main/java/life/majiang/community/community/mapper/UserMapper.java
+++ src/main/java/life/majiang/community/community/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package life.majiang.community.community.mapper;
 import life.majiang.community.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 /**
  * @ProjectName: community
@@ -19,4 +21,7 @@ public interface UserMapper {
 
     @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+
+    @Select("select * from user where token=#{token}")
+    User findByToken(@Param("token") String token);
 }
